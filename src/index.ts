c5b531a8f1a4e226632709b5f1b21e9e576ace36
valerii_v <volkovskiy.v@gmail.com>
2024-01-25 19:23:51 +0300

    Mutation also can execute listeners

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as t from './types.js'
 const statesMap: t.GlobalListeners = new WeakMap();
 let currentListener: t.Listener | null;
 const listenersQueue = createListenersSet();
+let isBulk = false;
 
 
 function getObservedState(instance: State<object>): t.PropertiesMap {
@@ -28,11 +29,12 @@ function createProxy<T extends object>(rawObj: T, stateInstance: State<T>): T {
       // todo: rename
       const $obj = getObservedState(stateInstance);
 
-      // console.log("SET: ", property);
-
       if ($obj.has(property)) {
-        // $obj.get(property)?.forEach((callback) => callback());
-        $obj.get(property)?.forEach((callback) => listenersQueue.add(callback));
+        if (isBulk) {
+          $obj.get(property)?.forEach((callback) => listenersQueue.add(callback));
+        } else {
+          $obj.get(property)?.forEach((callback) => callback());
+        }
       }
 
       return result;
@@ -70,13 +72,14 @@ class State<T extends object> {
   }
 
    #bulkUpdate = (cb: Function) => {
+    isBulk = true
     cb();
     listenersQueue.forEach(callback => callback());
     listenersQueue.clear();
+    isBulk = false
   }
 
   public setState = (newValue: Partial<T>) => {
-    // const state = this.getState();
     this.#bulkUpdate(() => {
       Object.assign(this.#state, newValue);
     })
@@ -108,61 +111,14 @@ class State<T extends object> {
 
 // DEBUG:
 
-const appleObj = { apples: 0, price: 0, points: [0] };
-const appleState = new State(appleObj);
 
-console.log(appleState.getState());
-
-
-
-function AlldependencyListener() {
-  const apples = appleState.getState().apples;
-  const price = appleState.getState().price;
-  const points = appleState.getState().points;
-  console.log("SUBSCRIBER 1 EXECUTED!", apples, price, points);
-}
-
-function noDepsListener() {
-  console.log("NO DEPENDENCY 2 SUB EXECUTED!");
-}
-function appleDep() {
-  console.log('apple dep', appleState.getState().apples)
-}
-function pointsDep() {
-  console.log('points dep', appleState.getState().points)
-}
-function priceDep() {
-  console.log('price dep', appleState.getState().price)
-}
-
-
-console.log("SET  ALL!")
-appleState.setState({apples: 100, price: 400, points: []});
-
-console.log('ALL STATE', appleState.getState());
-
-appleState.runEffect(AlldependencyListener);
-appleState.runEffect(noDepsListener);
-appleState.runEffect(appleDep);
-appleState.runEffect(pointsDep);
-appleState.runEffect(priceDep);
-
-
-console.log("SET POINTS")
-appleState.setState({points: [1,2]})
-
-
-console.log("SETUP ONLY APPLES");
-appleState.getState().apples = 999;
-
-
-
-// todo: setState
+// todo write tests
 // todo: unsubscribe
-// todo: computed()
-// todo: mutable/immutable options
-// todo: array & object changes
-// todo: no dependency subscription
 // todo: cancel side effects from listeners
+// todo: mutable/immutable options
+// todo: array & object changes?
+// todo: no dependency subscription (log everything?)
+// todo: add middlewares
+// todo: persistance
 
 export default State;
